cb6303ba719c59c586e56a73c54f088e40382187
Elliot Ros <ellliotros@gmail.com>
2022-12-09 18:10:56 +0100

    Add a reward for destroying enemies
    The player receives a flag (a new level marker) if he destroys 15 enemies

--- Assets/Scripts/DestroyEnemy.cs
+++ Assets/Scripts/DestroyEnemy.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DestroyEnemy : MonoBehaviour
 {
+    private int enemiesNextLevel = 15;
+    private static int enemiesCounter = 0;
+
+    public UnityEvent enableTheFlag;
+
     private void OnTriggerEnter2D(Collider2D fire)
     {
         GameManager.Instance.enemyDestroyed = true;
         this.gameObject.SetActive(false);
+        enemiesCounter++;
+
+        if (enemiesCounter % enemiesNextLevel == 0)
+        {
+            enableTheFlag.Invoke();
+        }
     }
 }
--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class UIHandler : MonoBehaviour
 {
     public GameObject lifeUI;
+    public GameObject flagsUI;
     public int livesCounter = 0;
+    public int flagsCounter = 0;
     
     public void DecreaseLife()
     {
         GameObject currentLifeUI = lifeUI.transform.GetChild(livesCounter).gameObject;
         currentLifeUI.SetActive(false);
         livesCounter++;
+    }
+
+    public void ToNewLevel()
+    {
+        GameObject currentFlagUI = flagsUI.transform.GetChild(flagsCounter).gameObject;
+        currentFlagUI.SetActive(true);
+        flagsCounter++;
     }
 }
