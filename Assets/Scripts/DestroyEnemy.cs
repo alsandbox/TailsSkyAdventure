d8b4b7bc77efd9b8aee8188d2d97f7ec41d5c229
Elliot Ros <ellliotros@gmail.com>
2023-01-02 15:53:54 +0100

    Update the DestroyEnemy script
    Added new logic, so that the flag appears after defeating every 15 enemies. The victory screen appears after successfully passing the 4th wave of enemies. Also added an animation and opacity check in OnTriggerExit2D, if the enemy is deactivated before the animation plays.

--- Assets/Scripts/DestroyEnemy.cs
+++ Assets/Scripts/DestroyEnemy.cs
@@ -1,24 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class DestroyEnemy : MonoBehaviour
 {
-    private int enemiesNextLevel = 15;
+    private readonly int enemiesNextLevel = 15;
+    private readonly int numberOfLevels = 4;
     private static int enemiesCounter = 0;
 
-    public UnityEvent enableTheFlag;
+    private Animator blink;
+    private SpriteRenderer spriteRenderer;
+
+    public GameObject winScreenUI;
+
+    public UnityEvent IncreaseFlagsEvent;
+
+
+    public EnemyController enemyController;
+
+    private void Start()
+    {
+        blink = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        enemyController = enemyController.GetComponent<EnemyController>();
+    }
 
     private void OnTriggerEnter2D(Collider2D fire)
     {
-        GameManager.Instance.enemyDestroyed = true;
-        this.gameObject.SetActive(false);
+        enemyController.IsDestroyed = true;
+        blink.SetTrigger("getsHit");
+        
         enemiesCounter++;
 
-        if (enemiesCounter % enemiesNextLevel == 0)
+        if (enemiesCounter % enemiesNextLevel == 0 & enemiesCounter < numberOfLevels * enemiesNextLevel)
         {
-            enableTheFlag.Invoke();
+            IncreaseFlagsEvent?.Invoke();
+        }
+        else if(enemiesCounter == numberOfLevels * enemiesNextLevel)
+        {
+            winScreenUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (spriteRenderer.color != Color.white)
+        { 
+            spriteRenderer.color = Color.white; 
         }
     }
 }
