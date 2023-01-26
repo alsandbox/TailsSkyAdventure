1285fedab8a76a0f24c3b067a7ccb11fd6173b68
Elliot Ros <ellliotros@gmail.com>
2023-01-26 12:42:32 +0100

    Create methods for the Game Over and Win screens
    Created methods for the Game Over and Win screens, and added sounds. Created Restart and Exit methods for buttons.

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIHandler : MonoBehaviour
 {
@@ -12,18 +14,36 @@ public class UIHandler : MonoBehaviour
 
     private AudioSource newLevelSound;
     private AudioSource gameOverAudioSource;
+    private AudioSource winAudioSource;
 
     private Animator gameOverAnimator;
+    private Animator winAnimator;
 
     private float gameOverDelay;
+    private float winDelay;
 
     public GameObject GameOverScreen;
+    public GameObject WinScreen;
+    public GameObject tails;
+
+    private GameObject buttonsGameOver;
+    private GameObject buttonsWin;
+
+    private int sceneID;
 
     private void Start()
     {
+        sceneID = SceneManager.GetActiveScene().buildIndex;
+
         gameOverAudioSource = GameOverScreen.GetComponent<AudioSource>();
         gameOverDelay = gameOverAudioSource.clip.length;
         gameOverAnimator = GameOverScreen.GetComponent<Animator>();
+        buttonsGameOver = GameOverScreen.transform.GetChild(GameOverScreen.transform.childCount - 1).gameObject;
+
+        winAudioSource = WinScreen.GetComponent<AudioSource>();
+        winDelay = winAudioSource.clip.length;
+        winAnimator = WinScreen.GetComponent<Animator>();
+        buttonsWin = WinScreen.transform.GetChild(WinScreen.transform.childCount - 1).gameObject;
     }
 
     public void DecreaseLife()
@@ -48,9 +68,46 @@ public class UIHandler : MonoBehaviour
         StartCoroutine(MusicDelay(gameOverDelay));
     }
 
+    public void Win()
+    {
+        WinScreen.SetActive(true);
+        tails.SetActive(false);
+        StartCoroutine(MusicDelay(winDelay));
+    }
+
+    public void Restart()
+    {
+        PlayerController.IsGameOver = false;
+        SceneManager.LoadScene(sceneID);
+    }
+
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+                EditorApplication.ExitPlaymode();
+        #elif (UNITY_WEBGL)
+                    Application.OpenURL("about:blank");
+        #else
+                    Application.Quit();
+        #endif
+    }
+
     IEnumerator MusicDelay(float clipLength)
     {
-        yield return new WaitForSeconds(clipLength);
-        gameOverAnimator.enabled = false;
+        if (GameOverScreen.activeSelf)
+        {
+            yield return new WaitForSeconds(clipLength / 2);
+            buttonsGameOver.SetActive(true);
+
+            yield return new WaitForSeconds(clipLength / 2);
+            gameOverAnimator.enabled = false;
+        }
+
+        if (WinScreen.activeSelf)
+        {
+            yield return new WaitForSeconds(clipLength);
+            winAnimator.enabled = false;
+            buttonsWin.SetActive(true);
+        }
     }
 }
